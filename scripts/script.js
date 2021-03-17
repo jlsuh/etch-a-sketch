0226e7ae4ff42e2c4135d5d1b0dd2af2287498fe
jlsuh <joelleonardosuh@gmail.com>
2021-03-17 11:01:23 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@
 
 /* Color generators */
 String.prototype.convertToRGB = function(){
-  var aRgbHex = this.match(/.{1,2}/g);
-  var aRgb = [
+  let aRgbHex = this.match(/.{1,2}/g);
+  let aRgb = [
     parseInt(aRgbHex[0], 16),
     parseInt(aRgbHex[1], 16),
     parseInt(aRgbHex[2], 16)
